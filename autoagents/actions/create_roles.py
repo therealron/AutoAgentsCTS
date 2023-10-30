df8e9596c8ba3fe23ca21b822b9e596780f0e1d2
rohan <admin@Admins-MacBook-Pro.local>
2023-10-29 20:15:57 -0400

    wip added klarna chain as tool

--- autoagents/actions/create_roles.py
+++ autoagents/actions/create_roles.py
@@ -117,7 +117,8 @@ OUTPUT_MAPPING = {
 # for item in TOOLS_LIST:
 #     TOOLS += '(Tool:' + item['toolname'] + '. Description:' + item['description'] + '),'
 # TOOLS += ']'
-TOOLS = 'tool: SearchAndSummarize, description: useful for when you need to answer unknown questions'
+TOOLS = '(tool: SearchAndSummarize, description: useful for when you need to answer unknown questions),'
+TOOLS += '(tool: SearchProductsFromKlarna, description: useful for when you want to search for ecommerce products like clothes, electronics, toys or any other product )'
 
 
 class CreateRoles(Action):
